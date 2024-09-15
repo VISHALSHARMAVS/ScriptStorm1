f3f88d223584fbca53305c316a29c8ab1d2ffa09
VISHALSHARMAVS <vishalsharmakt143@gamil.com>
2024-09-15 11:45:55 +0530

    remove unneccesary comments

--- backend/index.js
+++ backend/index.js
@@ -10,22 +10,6 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 
-// Define allowed origins
-// const allowedOrigins = ['https://scriptstorm1-1.onrender.com/', 'http://localhost:5173'];
-
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     // Allow requests with no origin (like mobile apps or Postman)
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//   credentials: true
-// };
-
 // Apply CORS middleware
 app.use(cors({origin:'https://scriptstorm1-1.onrender.com', credentials:true , methods:["GET","HEAD","PUT","PATCH","POST","DELETE"], allowedHeaders:'content-type' }));
 
@@ -38,12 +22,6 @@ app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/user', userRoute);
 app.use('/api/v1/post', postRoute);
 app.use('/api/v1/comment', commentRoutes);
-
-// app.use(express.static(path.join(__dirname, '/frontend/dist')));
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
-// });
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
