4ade33d9b646363759e6d2228f276111604b9bf9
VISHALSHARMAVS <vishalsharmakt143@gamil.com>
2024-09-15 21:46:14 +0530

    error debug

--- frontend/src/components/PostCard.jsx
+++ frontend/src/components/PostCard.jsx
@@ -16,7 +16,7 @@
             <p className='text-lg font-semibold line-clamp-2'>{post.title}</p>
             <span className='italic text-sm'>{post.category}</span>
             
-            <span className='italic py-1 text-sm font-semibold'>Author : @ {post.userId.username || 'Unkonwn author'}</span>
+            <span className='italic py-1 text-sm font-semibold'>Author : @ {post.userId?.username || 'Unkonwn author'}</span>
             <Link
             to={`/post/${post.slug}`}
             className='z-10 group-hover:bottom-0 absolute bottom-[-200px] left-0 right-0 border border-teal-500 text-teal-500 hover:bg-teal-500 hover:text-white transition-all duration-300 text-center py-2 rounded-md !rounded-tl-none m-2'
